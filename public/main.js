e75a3799f0ee4856314f2f21834a87eb8c2ed350
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-06-16 10:18:53 +0200

    Logo with animation in sticky nav.

--- public/main.js
+++ public/main.js
@@ -1,4 +1,6 @@
 window.addEventListener("scroll", function(){
     const nav = this.document.querySelector(".navigation");
+    const logo = document.querySelector(".navigation__logo");
     nav.classList.toggle("navigation--sticky", window.scrollY > 0);
-})
+    logo.classList.toggle("navigation__logo--visible", window.scrollY > 0);
+});
